2ec51f589b8a1dd51e957630d9bc5d1746954134
Rockthor1106 <jan.carvajal1@u.icesi.edu.co>
2023-04-25 17:04:45 -0500

    I set the values of our databse in
    coffeemach-user.sql

--- scripts/postgres/coffeemach-user.sql
+++ scripts/postgres/coffeemach-user.sql
@@ -1,6 +1,6 @@
-create user cofmachu with password 'cofmachpwd';
-create database coffeemachine owner cofmachu;
-grant connect on database coffeemachine to cofmachu;
+create user carvicoffeemach with password 'carvi';
+create database carvi_coffeemachine owner carvicoffeemach;
+grant connect on database carvi_coffeemachine to carvicoffeemach;
 
 -- Connection
 -- psql -d coffeemachine -U p09713_1_2
