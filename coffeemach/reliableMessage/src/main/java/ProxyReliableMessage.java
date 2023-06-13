f1e312495ec4877dc277eb6b3b493781922f7225
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2023-06-13 15:46:14 -0500

    Maybe that way it connects

--- coffeemach/reliableMessage/src/main/java/ProxyReliableMessage.java
+++ coffeemach/reliableMessage/src/main/java/ProxyReliableMessage.java
@@ -1,3 +1,27 @@
+import gateway.ReliableMessagePrx;
+
 public class ProxyReliableMessage {
-    
+    public static void main(String[] args) {
+        List<String> extPar = new ArrayList<>();
+
+        try (Communicator communicator = Util.initialize(args, "ReliableMessage.cfg", extPar)) {
+
+            ObservablePrx gateway = ReliableMessagePrx.checkedCast(communicator.propertyToProxy("gateway")).ice_twoway();
+
+            ObjectAdapter adapter = communicator.createObjectAdapter("ReliableMessage");
+            
+            ObservableImp proxyRM = new ObservableImp();
+            proxyRM.setCommunicator(communicator);
+
+            adapter.add(proxyRM, Util.stringToIdentity("ProxyRM"));
+
+            ProxyReliableMessageImp proxyReliableMessageImp = new ProxyReliableMessageImp(gateway);
+
+            ObjectPrx objectPrx = adapter.add(proxyReliableMessageImp, Util.stringToIdentity("proxyCache"));
+
+            adapter.activate();
+
+            communicator.waitForShutdown();
+        }
+    }
 }
