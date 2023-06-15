a09f3479499cbe6a7cc747926c3118cc2a9708be
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2023-06-15 15:26:46 -0500

    Update deploy.sh

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -6,32 +6,23 @@ path="JJJG-CoffeeMach"
 function gradleBuild {
     id=$1
     newMachineId=$id
-    sed -i "s|^CoffeMach.Endpoints = default -h localhost -p 9096|CoffeMach.Endpoints=default -h hgrid$newMachineId -p 9096|" ../../coffeemach/coffeeMach/src/main/resources/coffeMach.cfg
+    sed -i "s|^CoffeMach.Endpoints=default -h hgrid7 -p 9097|CoffeMach.Endpoints=default -h hgrid$newMachineId -p 9097|" ../../coffeemach/coffeeMach/src/main/resources/coffeMach.cfg
+
+    sed -i "s|^ProxyRM.Endpoints = tcp -h hgrid7 -p 9096|ProxyRM.Endpoints = tcp -h hgrid$newMachineId -p 9096|" ../../coffeemach/reliableMessage/src/main/resources/ReliableMessage.cfg
 
     sed -i "s/^1/$newMachineId/" ../../coffeemach/coffeeMach/codMaquina.cafe
     ../../coffeemach/./gradlew build -p ../../coffeemach/coffeeMach &
     wait
 
     echo "Modified conf file: coffeemach/coffeeMach/src/main/resources/coffeMach.cfg"
-    echo "New CoffeMach.Endpoints line: CoffeMach.Endpoints=default -h hgrid$newMachineId -p 9096"
+    echo "New CoffeMach.Endpoints line"
+    echo "Modified conf file: coffeemach/reliableMessage/src/main/resources/ReliableMessage.cfg"
+    echo "New ProxyRM.Endpoints"
 }
 
 IFS=',' read -ra array_machines <<< "$1"
 
 gradleBuild ${array_machines[0]}
-
-server_id="swarch@xhgrid3"
-echo "Deploying to $server_id"
-sshpass -p $password ssh $server_id "rm -rf $path && mkdir -p $path"
-sshpass -p $password scp ../../coffeemach/ServidorCentral/build/libs/ServidorCentral.jar $server_id:./$path
-
-echo "Deploying to xhgrid9"
-sshpass -p $password ssh swarch@xhgrid9 "rm -rf $path && mkdir -p $path"
-sshpass -p $password scp ../../coffeemach/reliableMessage/build/libs/reliableMessage.jar swarch@xhgrid9:./$path
-
-echo "Deploying to xhgrid7"
-sshpass -p $password ssh swarch@xhgrid7 "rm -rf $path && mkdir -p $path"
-sshpass -p $password scp ../../coffeemach/ProxyCache/build/libs/ProxyCache.jar swarch@xhgrid7:./$path
 
 for ((i = 0; i < ${#array_machines[@]}; i++)); do
     machine=${array_machines[i]}
